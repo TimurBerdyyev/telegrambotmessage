248e87963489fca84d737369df96bcd5e8e03221
TimurBerdyyev <gorilazanimals@gmail.com>
2024-03-12 14:34:13 +0600

    update nasa_foto.py and epic_image.py

--- epic_image.py
+++ epic_image.py
@@ -1,27 +1,22 @@
 import os
 import requests
-
 from dotenv import load_dotenv
 
 def get_epic_images(api_key, count=5):
     try:
-        url_natural_images = f'https://api.nasa.gov/EPIC/api/natural/images?api_key={api_key}'
-        response = requests.get(url_natural_images)
+        url_natural_images = 'https://api.nasa.gov/EPIC/api/natural/images'
+        params = {'api_key': api_key}
+        response = requests.get(url_natural_images, params=params)
         response.raise_for_status()
-
         response_data = response.json()
         if response_data:
             os.makedirs('images', exist_ok=True)
             for image_info in response_data[:count]:
                 image_date = image_info['date'][:10].replace('-', '/')
                 image_name = image_info['image']
-                image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png?api_key={api_key}"
-
-                # Скачиваем изображение
-                response = requests.get(image_url)
+                image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png"
+                response = requests.get(image_url, params=params)
                 response.raise_for_status()
-
-                # Сохраняем изображение
                 with open(f'images/{image_name}.png', 'wb') as f:
                     f.write(response.content)
                     print(f"Изображение {image_name}.png успешно сохранено")
--- nasa_foto.py
+++ nasa_foto.py
@@ -1,20 +1,21 @@
 import os
 from dotenv import load_dotenv
-from urllib.parse import urlparse
 import requests
 
 
 def fetch_nasa_image(api_key, count=5):
     try:
-        url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&count={count}'
-        response = requests.get(url)
+        url = 'https://api.nasa.gov/planetary/apod'
+        params = {'api_key': api_key, 'count': count}
+
+        response = requests.get(url, params=params)
         response.raise_for_status()
 
         apod_data = response.json()
         os.makedirs('images', exist_ok=True)
         for idx, image_data in enumerate(apod_data):
             image_url = image_data['url']
-            extension = os.path.splitext(urlparse(image_url).path)[-1]
+            extension = os.path.splitext(os.path.basename(image_url))[-1]
             with open(f'nasa_images/nasa_image{idx + 1}{extension}', 'wb') as f:
                 f.write(requests.get(image_url).content)
                 print(f"Изображение {idx + 1} успешно сохранено в nasa_images/nasa_image{idx + 1}{extension}")
@@ -24,13 +25,8 @@ def fetch_nasa_image(api_key, count=5):
     except Exception as e:
         print(f'Произошла ошибка: {e}')
 
+
 if __name__ == '__main__':
     load_dotenv()
     api_key = os.environ['NASA_API_KEY']
     fetch_nasa_image(api_key, count=3)
-
-
-
-
-
-
