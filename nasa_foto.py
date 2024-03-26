e52da8dd1d10ae8a8427f66f63f44508417f5f35
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-03-26 16:58:01 +0600

    Update nasa_foto.py
    Удаление try except

--- nasa_foto.py
+++ nasa_foto.py
@@ -7,33 +7,24 @@ import logging
 logger = logging.getLogger(__file__)
 
 def fetch_nasa_images(api_key, count=5):
-    try:
-        url = 'https://api.nasa.gov/planetary/apod'
-        params = {'api_key': api_key, 'count': count}
+    url = 'https://api.nasa.gov/planetary/apod'
+    params = {'api_key': api_key, 'count': count}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
 
-        response = requests.get(url, params=params)
-        response.raise_for_status()
+    content = response.json()
+    os.makedirs('nasa_images', exist_ok=True)
 
-        if response.status_code != 200:
-            logger.error(f"Ошибка при получении данных: {response.status_code}")
-            return
+    for index, image_info in enumerate(content):
+        image_url = image_info['url']
+        extension = os.path.splitext(os.path.basename(image_url))[-1]
 
-        content = response.json()
+        with open(f'nasa_images/nasa_image{index + 1}{extension}', 'wb') as f:
+            image_response = requests.get(image_url)
+            f.write(image_response.content)
 
-        os.makedirs('nasa_images', exist_ok=True)
+        logger.info(f"Изображение {index + 1} успешно сохранено в nasa_images/nasa_image{index + 1}{extension}")
 
-        for index, image_info in enumerate(content):
-            image_url = image_info['url']
-            extension = os.path.splitext(os.path.basename(image_url))[-1]
-
-            with open(f'nasa_images/nasa_image{index + 1}{extension}', 'wb') as f:
-                image_response = requests.get(image_url)
-                f.write(image_response.content)
-
-            logger.info(f"Изображение {index + 1} успешно сохранено в nasa_images/nasa_image{index + 1}{extension}")
-
-    except requests.RequestException as e:
-        logger.error(f"Ошибка при запросе: {e}")
 
 def main():
     logging.basicConfig(level=logging.ERROR)
