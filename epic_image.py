3ca0ae45269c1a8ad09561691f72b985497e81ca
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-03-26 16:19:58 +0600

    Update epic_image.py
    Изменил название переменной json_content на content

--- epic_image.py
+++ epic_image.py
@@ -11,14 +11,14 @@ def get_epic_images(api_key, count=5):
         params = {'api_key': api_key}
         response = requests.get(url_natural_images, params=params)
         response.raise_for_status()
-        json_content = response.json()
+        content = response.json()
 
-        if not json_content:
+        if not content:
             logger.info("Информация об изображениях отсутствует.")
             return
 
         os.makedirs('images', exist_ok=True)
-        for image_info in json_content[:count]:
+        for image_info in content[:count]:
             image_date = image_info['date'][:10].replace('-', '/')
             image_name = image_info['image']
             image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png"
