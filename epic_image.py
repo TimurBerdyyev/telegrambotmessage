b245e20dac095eb88574ff00ae673f6c5457f351
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-03-26 16:30:57 +0600

    Update epic_image.py
    Изменение строки for image_info in json_content[:count]: на for image_data in content[:count]:

--- epic_image.py
+++ epic_image.py
@@ -18,9 +18,9 @@ def get_epic_images(api_key, count=5):
             return
 
         os.makedirs('images', exist_ok=True)
-        for image_info in content[:count]:
-            image_date = image_info['date'][:10].replace('-', '/')
-            image_name = image_info['image']
+        for image_data in content[:count]:
+            image_date = image_data['date'][:10].replace('-', '/')
+            image_name = image_data['image']
             image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png"
             response = requests.get(image_url, params=params)
             response.raise_for_status()
