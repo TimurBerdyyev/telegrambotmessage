d8bf601583483c7c29b03d7345aa842ae10f69a5
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-03-26 18:06:18 +0600

    Update epic_image.py
    Убрал перехватчик ошибок

--- epic_image.py
+++ epic_image.py
@@ -8,29 +8,25 @@ logger = logging.getLogger(__file__)
 
 
 def get_epic_images(api_key, count=5):
-    try:
-        url_natural_images = 'https://api.nasa.gov/EPIC/api/natural/images'
-        params = {'api_key': api_key}
-        response = requests.get(url_natural_images, params=params)
-        response.raise_for_status()
-        content = response.json()
+    url_natural_images = 'https://api.nasa.gov/EPIC/api/natural/images'
+    params = {'api_key': api_key}
+    response = requests.get(url_natural_images, params=params)
+    response.raise_for_status()
+    content = response.json()
 
-        if not content:
-            logger.info("Информация об изображениях отсутствует.")
-            return
-        os.makedirs('images', exist_ok=True)
+    if not content:
+        logger.info("Информация об изображениях отсутствует.")
+        return
+    os.makedirs('images', exist_ok=True)
 
-        for image_data in content[:count]:
-            image_date = image_data['date'][:10].replace('-', '/')
-            image_name = image_data['image']
-            image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png"
-            filename = f'images/{image_name}.png'
-            fetch_data_and_save(image_url, filename)
+    for image_data in content[:count]:
+        image_date = image_data['date'][:10].replace('-', '/')
+        image_name = image_data['image']
+        image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png"
+        filename = f'images/{image_name}.png'
+        fetch_data_and_save(image_url, filename)
 
-            logger.info(f"Изображение {image_name}.png успешно сохранено")
-
-    except requests.RequestException as e:
-        logger.error(f"Ошибка при запросе: {e}")
+        logger.info(f"Изображение {image_name}.png успешно сохранено")
 
 
 def setup_logging():
