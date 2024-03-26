d390e38bb9ecc220236765d65af6b522abf2a2d9
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-03-26 16:45:42 +0600

    Update epic_image.py
    Изменение настройки loger и выведение его в отдельную функцию плюс добавление в его в if __name__ = '__main__'

--- epic_image.py
+++ epic_image.py
@@ -1,12 +1,11 @@
-import os
-import requests
-from dotenv import load_dotenv
-from logger_config import getLogger
+import logging
 
 
-logger = getLogger()
+logger = logging.getLogger(__file__)
+
 
 def get_epic_images(api_key, count=5):
+    try:
         url_natural_images = 'https://api.nasa.gov/EPIC/api/natural/images'
         params = {'api_key': api_key}
         response = requests.get(url_natural_images, params=params)
@@ -16,8 +15,8 @@ def get_epic_images(api_key, count=5):
         if not content:
             logger.info("Информация об изображениях отсутствует.")
             return
-
         os.makedirs('images', exist_ok=True)
+
         for image_data in content[:count]:
             image_date = image_data['date'][:10].replace('-', '/')
             image_name = image_data['image']
@@ -29,8 +28,16 @@ def get_epic_images(api_key, count=5):
 
             logger.info(f"Изображение {image_name}.png успешно сохранено")
 
+    except requests.RequestException as e:
+        logger.error(f"Ошибка при запросе: {e}")
+
+
+def setup_logging():
+    logging.basicConfig(level=logging.ERROR)
+    logger.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
     load_dotenv()
     api_key = os.environ['NASA_API_KEY']
+    setup_logging()
     get_epic_images(api_key, count=5)
