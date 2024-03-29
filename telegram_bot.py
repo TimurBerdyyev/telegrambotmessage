f937a588069c2c3c65699e955b2aee6f97249609
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-03-29 11:04:31 +0600

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -24,9 +24,7 @@ def publish_photos():
             bot.send_photo(chat_id=TELEGRAM_CHANNEL_ID, photo=photo_file)
         time.sleep(10)
 
-def main():
-    publish_photos()
 
 if __name__ == "__main__":
     load_dotenv()
-    main()
+    publish_photos()
