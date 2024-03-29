d924a39cc332b879a3d23f08b5964ccca588ee02
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-03-29 10:37:47 +0600

    Update fetch_data_and_save.py

--- fetch_data_and_save.py
+++ fetch_data_and_save.py
@@ -1,5 +1,5 @@
 import requests
 
-def download_and_save(url, filename):
-    with open(filename, 'wb') as f:
+def download_and_save(url, image_path):
+    with open(image_path, 'wb') as f:
         f.write(requests.get(url).content)
