f8ca51477acba4d0e3560672f61a4e7166ee84c5
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-03-29 10:40:17 +0600

    Update nasa_foto.py
    исправление ошибое

--- nasa_foto.py
+++ nasa_foto.py
@@ -21,12 +21,12 @@ def fetch_nasa_images(api_key, count=5):
         image_url = image_info['url']
         extension = os.path.splitext(os.path.basename(image_url))[-1]
     
-        filename = f'nasa_images/nasa_image{image_index}{extension}'
-        download_and_save(image_url, filename)
+        image_path = f'nasa_images/nasa_image{image_index}{extension}'
+        download_and_save(image_url, image_path)
         
 
 
-        logger.info(f"Изображение {image_index} успешно сохранено в nasa_images/nasa_image{image_index + 1}{extension}")
+        logger.info(f"Изображение {image_index} успешно сохранено в nasa_images/nasa_image{image_index}{extension}")
 
 
 def main():
