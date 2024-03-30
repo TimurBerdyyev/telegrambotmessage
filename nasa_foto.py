8266b5b55d3f8d23a57f9fea7d72f0076e251f82
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-03-30 16:45:15 +0600

    Update nasa_foto.py

--- nasa_foto.py
+++ nasa_foto.py
@@ -17,16 +17,16 @@ def fetch_nasa_images(api_key, count=5):
     content = response.json()
     os.makedirs('nasa_images', exist_ok=True)
 
-    for image_index, image_info in enumerate(content, start=1):
-        image_url = image_info['url']
+    for index_image, data_image in enumerate(content, start=1):
+        image_url = data_image['url']
         extension = os.path.splitext(os.path.basename(image_url))[-1]
     
-        image_path = f'nasa_images/nasa_image{image_index}{extension}'
+        image_path = f'nasa_images/nasa_image{index_image}{extension}'
         download_and_save(image_url, image_path)
         
 
 
-        logger.info(f"Изображение {image_index} успешно сохранено в {image_path}")
+        logger.info(f"Изображение {index_image} успешно сохранено в {image_path}")
 
 
 def main():
