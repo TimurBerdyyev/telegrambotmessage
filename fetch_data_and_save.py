4428667dbb7cc42094c5de0b39e3834a8d87890f
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-04-02 10:16:13 +0600

    Update fetch_data_and_save.py
    Убрал перехватчик исключений

--- fetch_data_and_save.py
+++ fetch_data_and_save.py
@@ -3,10 +3,8 @@ import requests
 def download_and_save(url, image_path):
     response = requests.get(url)
     
-    try:
-        response.raise_for_status()
-        with open(image_path, 'wb') as f:
-            f.write(response.content)
-        print("Файл успешно загружен и сохранен.")
-    except requests.exceptions.HTTPError as error:
-        print(f"Ошибка загрузки файла: {error}")
+    response.raise_for_status()
+    with open(image_path, 'wb') as f:
+        f.write(response.content)
+    print("Файл успешно загружен и сохранен.")
+    
